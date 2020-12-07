4a2cc0302edd6c47d2c4ac3b7bc05c10b43b06d9
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2020-12-07 17:49:31 -0500

    new

--- scripts/click_handlers.js
+++ scripts/click_handlers.js
@@ -1,25 +1,25 @@
-/* TODO: Program the following: 
+/* TODO: Program the following:
  *  Finish each helper function below.
  *
  * 1. Finish attachClickHandlers().
  *      HINT: It looks like someone forgot to add a click handler to the Jeopardy questions!
  *
  * 2. Check to make sure that your code change works.
- *      HINT: Launch the game and click on a question. If you see a pop-up with the question, category and value, then it worked! 
+ *      HINT: Launch the game and click on a question. If you see a pop-up with the question, category and value, then it worked!
  *            Your screen should look like {LINK}.
  *      Need more help? Ask a volunteer.
- * 
+ *
  * 3. Finish questionOnClick().
  *      HINT: It looks like the previous engineer also forgot to add a click handler for displaying the answer in the jeopardy box.
- * 
+ *
  * 4. Check to make sure that your code change works.
- *      HINT: Launch the game and click on a question. Then click on "show answer". If the answer is displayed, then it worked! 
+ *      HINT: Launch the game and click on a question. Then click on "show answer". If the answer is displayed, then it worked!
  *            Your screen should look like {LINK}.
  *      Need more help? Ask a volunteer.
- * 
+ *
  * 5. Challenge!!!!! Fix the "close" button.
  *      HINT: Read through the code and identify where the click handler is added to the close button.
- * 
+ *
  * 6. Check to make sure that your code change works.
  *      HINT: Launch the game and click on a question, click on "show answer" and finally click on "close".
  *            If the popup closes correctly, then it worked!
@@ -27,13 +27,13 @@
  */
 
 /* TODO: Program the following:
-*   Attach a click handler to each question on the Jeopardy board.
-*
-* 1. Call the questionOnClick helper function when $(questionElement) is clicked.
-*      HINT: The questionOnClick takes two parameters. 
-*      Find out which ones by looking at the function.
-*      Need more help? Check out https://bit.ly/2RP3RBg or ask a volunteer.
-*/
+ *   Attach a click handler to each question on the Jeopardy board.
+ *
+ * 1. Call the questionOnClick helper function when $(questionElement) is clicked.
+ *      HINT: The questionOnClick takes two parameters.
+ *      Find out which ones by looking at the function.
+ *      Need more help? Check out https://bit.ly/2RP3RBg or ask a volunteer.
+ */
 function attachClickHandlers() {
   $(".question-cell").each((index, questionElement) => {
     // categoryIndex is used to look up the jeopardy question for this element.
@@ -41,17 +41,19 @@ function attachClickHandlers() {
     let moneyValue = $(questionElement).text().replace("$", "");
     $(questionElement).click(() => {
       // Add code here
+
+      questionOnClick(categoryIndex, moneyValue);
     });
   });
 }
 
 /* TODO: Program the following:
-*   Populate the question modal with data for the appropriate question.
-*
-* 1. Replace the console.log with the helper function that displays the answer.
-*      HINT: One of the helper methods in this file displays the answer. Find it!
-*      Need more help? Ask a volunteer.
-*/
+ *   Populate the question modal with data for the appropriate question.
+ *
+ * 1. Replace the console.log with the helper function that displays the answer.
+ *      HINT: One of the helper methods in this file displays the answer. Find it!
+ *      Need more help? Ask a volunteer.
+ */
 function questionOnClick(categoryIndex, moneyValue) {
   let question = getQuestion(categoryIndex, moneyValue);
   $("#question-modal-title").html(
@@ -61,12 +63,14 @@ function questionOnClick(categoryIndex, moneyValue) {
   $("#question-modal-show-answer")
     .click(() => {
       // Add code here
+      answerOnClick(categoryIndex, moneyValue);
       console.log("I just got clicked, I wish I could do more though!");
     })
     .show();
 
   $("#question-modal-close").click(() => {
     // Add code here
+    hideQuestionPopup();
     console.log("I just got clicked, I wish I could do more though!");
   });
   $("#question-modal").modal("show");
